6c074a85339ee7eb2c933efdddc4fe58e6201851
vivekyadavoffline <vivekbhiwadi704@gmail.com>
2026-03-18 19:11:56 +0530

    file restructuring

---
+++ react/component props/src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/footer"
+import Navbar from "./components/navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ react/component props/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+  return (
+    <div className="footer">
+      copyright @ 2024 | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
