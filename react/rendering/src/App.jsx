86048eb25b9c765015883a90349708b4daf94c94
vivekyadavoffline <vivekbhiwadi704@gmail.com>
2026-03-22 20:55:55 +0530

    BASIC UPDATE
    LEARNED TO ADD TAILWIND AND REACCT IN ONE

---
+++ react/rendering/src/App.jsx
@@ -0,0 +1,152 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+ const [ showBtn , setshowBtn] = useState(true)
+ const[ todos , settodos] = useState([
+  {
+    title:"first todo",
+    description:"this is the first todo",
+    status:"completed as first"
+  },
+  {
+    title:"second todo",
+    description:"this is the second todo",
+    status:"completed as second"
+  },
+  {
+    title:"third todo",
+    description:"this is the third todo",
+    status:"completed as third"
+  }])
+const Todo = (todo) => {
+  return (
+    <> <div class="m-4 border-1"></div>
+      <div> {todo.title} </div>
+      <div> {todo.description} </div>
+      <div> {todo.status} </div>
+    </>
+  )
+}
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div> 
+        {/* {showBtn ? <button> i will be shown only if second button is clicked </button> : "thenga"} */}
+        { showBtn && <button> i will be shown only if second button is clicked </button>}
+        <button
+          className="counter"
+          onClick={() => setshowBtn(!showBtn)}
+        >
+          Count is {count}
+        </button>
+        {todos.map( todo => <Todo {...todo} />)}
+
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
