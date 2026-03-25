0e3e4b77bb46ca0b36dd1534fde829a6e2a825aa
vivekyadavoffline <vivekbhiwadi704@gmail.com>
2026-03-25 12:55:25 +0530

    LEARNED ABOU USECALL BACLK

---
+++ react/useCallback/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = ({Adjective
+    
+}) => { console.log("Navbar rendered")
+  return (
+    <div>
+      I am a{Adjective} Navbar
+    </div>
+  )
+}
+
+export default Navbar
