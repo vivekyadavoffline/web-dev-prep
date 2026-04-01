e87c22d58dcb35f35f2b56adc74daad5fca742d6
vivekyadavoffline <vivekbhiwadi704@gmail.com>
2026-04-01 21:59:12 +0530

    learned form handling

---
+++ react/formHandling/src/App.jsx
@@ -0,0 +1,29 @@
+import { useForm } from "react-hook-form";
+import './App.css';
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="form">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name")} type="text" />
+          <input {...register("email")} type="email" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
+export default App
